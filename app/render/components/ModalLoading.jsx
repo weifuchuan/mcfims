e5336a27c180a46b447d84b33da0655625e06788
weifuchuan <fuchuan.wei@hotmail.com>
2018-06-21 18:47:37 +0800

    ok

--- app/render/components/ModalLoading.jsx
+++ app/render/components/ModalLoading.jsx
@@ -1,6 +1,155 @@
 import React, { Component } from "react";
 
+/*
+
+.fc-loading {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.fc-loading-spinner {
+  width: 50px;
+  height: 60px;
+  text-align: center;
+  font-size: 10px;
+}
+
+.fc-loading-spinner > div {
+  background-color: #67CF22;
+  height: 100%;
+  width: 6px;
+  display: inline-block;
+
+  -webkit-animation: stretchdelay 1.2s infinite ease-in-out;
+  animation: stretchdelay 1.2s infinite ease-in-out;
+}
+
+.fc-loading-spinner .fc-loading-rect2 {
+  -webkit-animation-delay: -1.1s;
+  animation-delay: -1.1s;
+}
+
+.fc-loading-spinner .fc-loading-rect3 {
+  -webkit-animation-delay: -1.0s;
+  animation-delay: -1.0s;
+}
+
+.fc-loading-spinner .fc-loading-rect4 {
+  -webkit-animation-delay: -0.9s;
+  animation-delay: -0.9s;
+}
+
+.fc-loading-spinner .fc-loading-rect5 {
+  -webkit-animation-delay: -0.8s;
+  animation-delay: -0.8s;
+}
+
+@-webkit-keyframes stretchdelay {
+  0%, 40%, 100% {
+    -webkit-transform: scaleY(0.4)
+  }
+  20% {
+    -webkit-transform: scaleY(1.0)
+  }
+}
+
+@keyframes stretchdelay {
+  0%, 40%, 100% {
+    transform: scaleY(0.4);
+    -webkit-transform: scaleY(0.4);
+  }
+  20% {
+    transform: scaleY(1.0);
+    -webkit-transform: scaleY(1.0);
+  }
+}
+*/
+
 export default class ModalLoading extends Component {
+  static _insertedStyle = false;
+
+  static _insertStyle() {
+    if (!ModalLoading._insertedStyle) {
+      const style = document.createElement("style");
+      style.appendChild(document.createTextNode(
+        `
+        .fc-loading {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        
+        .fc-loading-spinner {
+          width: 50px;
+          height: 60px;
+          text-align: center;
+          font-size: 10px;
+        }
+        
+        .fc-loading-spinner > div {
+          background-color: #67CF22;
+          height: 100%;
+          width: 6px;
+          display: inline-block;
+        
+          -webkit-animation: stretchdelay 1.2s infinite ease-in-out;
+          animation: stretchdelay 1.2s infinite ease-in-out;
+        }
+        
+        .fc-loading-spinner .fc-loading-rect2 {
+          -webkit-animation-delay: -1.1s;
+          animation-delay: -1.1s;
+        }
+        
+        .fc-loading-spinner .fc-loading-rect3 {
+          -webkit-animation-delay: -1.0s;
+          animation-delay: -1.0s;
+        }
+        
+        .fc-loading-spinner .fc-loading-rect4 {
+          -webkit-animation-delay: -0.9s;
+          animation-delay: -0.9s;
+        }
+        
+        .fc-loading-spinner .fc-loading-rect5 {
+          -webkit-animation-delay: -0.8s;
+          animation-delay: -0.8s;
+        }
+        
+        @-webkit-keyframes stretchdelay {
+          0%, 40%, 100% {
+            -webkit-transform: scaleY(0.4)
+          }
+          20% {
+            -webkit-transform: scaleY(1.0)
+          }
+        }
+        
+        @keyframes stretchdelay {
+          0%, 40%, 100% {
+            transform: scaleY(0.4);
+            -webkit-transform: scaleY(0.4);
+          }
+          20% {
+            transform: scaleY(1.0);
+            -webkit-transform: scaleY(1.0);
+          }
+        }
+      `
+      ));
+      document.getElementsByTagName("head").item(0).appendChild(style);
+      ModalLoading._insertedStyle = true;
+    }
+  }
+
+  constructor(props) {
+    super(props);
+
+    ModalLoading._insertStyle();
+  }
+
+
   render() {
     let { loading, title } = this.props;
     title = title ? title : "加载中...";
@@ -29,34 +178,33 @@ export default class ModalLoading extends Component {
             padding: "15px"
           }}
         >
-          <div className="loading">
+          <div className="fc-loading">
             <div>
-              <div className="loading-spinner">
+              <div className="fc-loading-spinner">
                 <div
                   style={{ marginLeft: "2px", marginRight: "2px" }}
-                  className="loading-rect1"
+                  className="fc-loading-rect1"
                 />
                 <div
                   style={{ marginLeft: "2px", marginRight: "2px" }}
-                  className="loading-rect2"
+                  className="fc-loading-rect2"
                 />
                 <div
                   style={{ marginLeft: "2px", marginRight: "2px" }}
-                  className="loading-rect3"
+                  className="fc-loading-rect3"
                 />
                 <div
                   style={{ marginLeft: "2px", marginRight: "2px" }}
-                  className="loading-rect4"
+                  className="fc-loading-rect4"
                 />
                 <div
                   style={{ marginLeft: "2px", marginRight: "2px" }}
-                  className="loading-rect5"
+                  className="fc-loading-rect5"
                 />
               </div>
-              <div style={{ color: "#fff" }}>加载中...</div>
+              <div style={{ color: "#fff" }}>{title}</div>
             </div>
           </div>
-          <div>{title}</div>
         </div>
       </div>
     );
